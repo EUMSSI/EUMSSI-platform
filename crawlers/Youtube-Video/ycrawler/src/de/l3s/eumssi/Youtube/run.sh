d1ecde96dfc6f54511b3eaaae582cb57b163aaef
Giang Binh Tran <giangtranbinh@gmail.com>
2015-08-04 17:57:10 +0200

    bash scripts

---
+++ crawlers/Youtube-Video/ycrawler/src/de/l3s/eumssi/Youtube/run.sh
@@ -0,0 +1,17 @@
+pip install -r requirements.txt 
+COUNTER=0
+while [  $COUNTER -lt 100000 ]; do
+    echo The counter is $COUNTER
+    let COUNTER=COUNTER+1
+    for keyword in biofuels biomass bioenergy coal energy energy-monitoring hydraulic-fracturing \
+	fossil-fuels gas oil shale-gas shale-oil nuclear-waste solar-power nuclear-power wind-power hydrogen-power \
+	renewable-energy fracking
+    do
+		echo "search "  $keyword
+	python Pycrawler.py --q $keyword
+    done
+    sleep 10h
+done
+
+     
+	
