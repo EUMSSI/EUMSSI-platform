f9a578d8dd5d79ebb0214450a324b0ee6ce7c904
Jens Grivolla <j+github@grivolla.net>
2015-10-14 17:08:31 +0200

    preprocessing: extract amalia.js for indexing

---
+++ preprocess/video_persons2extracted.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from extract_converter import ExtractConverter
+import click
+
+
+def convert(x):
+    meta = {
+        #meta['all'] = [w['item'] for w in x['result']['content']]
+        'amalia': x['result']['Amalia_Json']])
+    }
+    available_data = ['video_persons']
+    return meta, available_data
+
+
+@click.command()
+@click.option('--reset', is_flag=True, help="reset data_available")
+@click.option('--clean', is_flag=True, help="reset data_available and remove existing meta.source")
+def run(reset, clean):
+    conv = ExtractConverter('video_persons', convert)
+    conv.run()
+
+
+if __name__ == '__main__':
+    run()
